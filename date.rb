ec27fc507d79c3ed90a38844debc1bd4354d464d
ecavazos <ejcavazos@gmail.com>
2010-06-15 22:56:20 -0700

    spelling

--- date.rb
+++ date.rb
@@ -8,7 +8,7 @@ describe 'Date' do
     Date.new(2010, 06, 14).strftime('%Y%m%d').must_equal '20100614'
   end
 
-  it 'should formate date and time (yyyymmddhhmmss)' do
+  it 'should format date and time (yyyymmddhhmmss)' do
     DateTime.new(2010, 06, 14, 15, 16, 17)
       .strftime('%Y%m%d%H%M%S').must_equal '20100614151617'
   end
